66c50ed875b7d7f86d1f8ee56b35b14b128e2e24
Oscar Broman <oscar.broman@gmail.com>
2013-05-31 13:25:15 +0200

    add run-amx binary

---
+++ bin/run-amx.js
@@ -0,0 +1,75 @@
+#!/usr/bin/env node
+
+'use strict';
+
+var path = require('path');
+var sampServer = require('../samp-server');
+
+var amx = process.argv[2];
+
+if (!amx) {
+  console.error('Usage: run-amx [file]');
+
+  process.exit();
+}
+
+amx = path.resolve(amx);
+
+var serverBinary;
+
+if (process.platform === 'win32' || process.platform === 'darwin') {
+  serverBinary = 'samp-server.exe';
+} else {
+  serverBinary = 'samp03svr';
+}
+
+serverBinary = path.join(__dirname, serverBinary);
+
+var quitting = false;
+
+['SIGQUIT', 'SIGTERM', 'SIGHUP', 'SIGINT', 'exit'].forEach(function(event) {
+  process.on(event, shutdown);
+});
+
+process.once('uncaughtException', function(e) {
+  shutdown();
+
+  throw e;
+});
+
+function shutdown() {
+  if (quitting) {
+    return;
+  }
+
+  quitting = true;
+
+  sampServer.closeAllSync('SIGKILL');
+}
+
+sampServer.tempServer(
+  path.resolve('test', 'server', 'gamemodes', 'test.amx'), {
+    binary: path.resolve('test', 'server', 'samp-server.exe')
+  }, function(err, server) {
+    if (err) throw err;
+
+    process.stdin.resume();
+    process.stdin.on('data', function(data) {
+      server.send(data.toString().trim());
+    });
+
+    server
+      .on('error', function(err) {
+        throw err;
+      })
+      .on('output', function(data) {
+        console.log(data.trim());
+      })
+      .on('stop', function() {
+        console.log('The server stopped');
+
+        process.exit();
+      })
+      .start();
+  }
+);
